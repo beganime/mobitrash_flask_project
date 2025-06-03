c8c1bb467bfab77c9d1273c3dde8d30b7810b5ab
beganime <begenchyagmurow2008@gmail.com>
2025-06-03 11:16:46 +0500

    обновил ещё несколько вещей

--- init_db.py
+++ init_db.py
@@ -22,52 +22,76 @@ def init_db():
 
         cur.execute("""
             CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) UNIQUE NOT NULL,
-                password VARCHAR(255) NOT NULL,
-                email VARCHAR(255) UNIQUE NOT NULL,
+                id serial NOT NULL,
+                name text NOT NULL,
+                email text NOT NULL,
+                password text NOT NULL,
+                role text NOT NULL DEFAULT 'user'::text,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         """)
 
         cur.execute("""
             CREATE TABLE IF NOT EXISTS product (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                price NUMERIC(10, 2) NOT NULL,
-                description TEXT,
+                id integer NOT NULL DEFAULT nextval('samsung_mobile_id_seq'::regclass),
                 image_url VARCHAR(255),
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                name text NOT NULL,
+                price integer NOT NULL,
+                description character(4000) NOT NULL,
+                procesor character(100) NOT NULL,
+                batarey integer NOT NULL,
+                ram integer NOT NULL,
+                rom integer NOT NULL,
+                display character(100),
+                camera character(100) NOT NULL,
+                "like" integer DEFAULT 0,
+                created_at timestamp without time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+                storage integer,
+                category_id integer NOT NULL
             );
         """)
 
         cur.execute("""
             CREATE TABLE IF NOT EXISTS basket (
-                id SERIAL PRIMARY KEY,
-                user_id INTEGER NOT NULL,
-                product_id INTEGER NOT NULL,
-                added_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                active BOOLEAN DEFAULT TRUE,
+                id serial NOT NULL,
+                user_id integer NOT NULL,
+                product_id integer NOT NULL,
+                added_date timestamp without time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+                active boolean NOT NULL DEFAULT TRUE,
                 FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
                 FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
             );
         """)
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS category (
+                id serial NOT NULL,
+                device character(50),
+                company character(50)
+            );
+        """)
 
-        # Опционально: вставка начальных данных (seed data)
-        # Это полезно для тестов или для того, чтобы приложение сразу было с какими-то данными
+        cur.execute("""
+            INSERT INTO users (name, password, email,role)
+            VALUES ('Bega', '123456789', 'bega@example.com','admin')
+            ON CONFLICT (name) DO NOTHING;
+        """)
         cur.execute("""
             INSERT INTO users (name, password, email)
-            VALUES ('testuser', 'your_hashed_password_here', 'test@example.com')
+            VALUES ('David', '1234567890', 'david@example.com')
             ON CONFLICT (name) DO NOTHING;
         """)
         cur.execute("""
-            INSERT INTO product (name, price, description)
-            VALUES ('Samsung Galaxy ZFold 4', 0.00, 'В линейке Samsung Galaxy Z Fold4...')
+            INSERT INTO product ( name, price, description, procesor, batarey, ram, rom, display, 
+            camera, "like", storage, category_id)
+            VALUES ( 'testphone1', 544, 'mobileblablbabla', 'fws2', 5000, 8, 128, 'amoled','3(10MP-8MP-4MP)', 
+            0, 10, 1)
             ON CONFLICT (name) DO NOTHING;
         """)
         cur.execute("""
-            INSERT INTO product (name, price, description)
-            VALUES ('iPhone 15 Pro', 999.99, 'Новейший флагман от Apple.')
+            INSERT INTO product ( name, price, description, procesor, batarey, ram, rom, display, 
+            camera, "like", storage, category_id)
+            VALUES ( 'testphone2', 234, 'mobileblablbabla', 'fws2', 5000, 8, 128, 'amoled','3(10MP-8MP-4MP)', 
+            0, 10, 1)
             ON CONFLICT (name) DO NOTHING;
         """)
 
